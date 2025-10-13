79daf8d6cfb23dd50413247292dbe2ee2f9e3cb9
user <redzuan.amar15@gmail.com>
2025-10-13 16:54:30 +0800

    kenapa tak jadi?

--- getData.R
+++ getData.R
@@ -1,9 +1,14 @@
 library(httr)
 library(jsonlite)
+url <- "https://www.reddit.com/r/SideProject/.json"
 
-r <- GET("https://www.reddit.com/r/Scams/.json")
+headers <- add_headers(
+    "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:143.0) Gecko/20100101 Firefox/143.0"
 
+)
+
+r <- GET(url, headers)
 c <- fromJSON(rawToChar (r$content), flatten = TRUE)
-c$data$after
 
 df <- c$data$children
+
